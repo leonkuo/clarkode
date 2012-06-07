8ba9aef25aeef8baf621edc04566b1c77a21d3f5
Clark J. Wang (ZPK) <dearvoid@gmail.com>
2012-06-07 11:42:08 +0800

    updated

--- tcl/using_no_braces.tcl
+++ tcl/using_no_braces.tcl
@@ -5,6 +5,8 @@
 # Can you program Tcl without using braces and double-quotes? This
 # short script helps you understand the Tcl language better.
 #
+# To show the magic, pass one parameter to the script.
+#
 #--------------------------------------------------------------------#
 
 \x70\162\157c\
@@ -12,4 +14,4 @@
 a\x72\147s\
 \146or\x20\x7b\163et\x20r\x20\061\175\x20\x7b\x24r\x3c=\071\175\x20\x7bincr\x20r\175\x20\173for\x20\173\163et\x20\x63\040\x31\175\x20\173\x24\143\x3c=\x24r\175\x20\173\151nc\162\x20\x63\175\x20\173p\x75t\163\x20-\156one\x77l\151ne\x20\x24\173c\175x\x24r=\x5b\x66or\155at\x20\x25-3d\x20\133e\x78pr\x20\x24\x63*\x24r\135\x5d\x3b\175\x3b\160u\x74s\x20\x22\042\x3b\175
 
-9x9
+if \[llength\ \$argv] puts\ \[concat\ \[list\ proc\ 9x9\ args\ \[info\ body\ 9x9]]]\;exit\; else 9x9
